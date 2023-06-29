4e7a528d46863b52af76a1d56c4914966983e144
Jacob Greene <jgreene97@att.net>
2023-06-29 16:17:36 -0400

    Add Animated Burger Icon

---
+++ js/mobile-nav.js
@@ -0,0 +1,25 @@
+/***********************
+*
+*	HTML QUERY SELECTION / VARIABLE DEFINITION
+*
+***********************/
+
+let headerMain = document.querySelector(".header-main-nav")
+let headerMainBurger = document.querySelector(".header-main-burger")
+
+let isMenuVisible = false;
+
+/***********************
+*
+*	BURGER MENU FUNCTIONALITY
+*
+***********************/
+
+headerMainBurger.onclick = function() {
+    if(!isMenuVisible){
+
+    }
+    else {
+
+    }
+}
